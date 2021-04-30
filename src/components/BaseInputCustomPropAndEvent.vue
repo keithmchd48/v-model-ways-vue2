b7b6145d4258459f261826fbd71f60ba611b1ed4
keithmchd48 <keithmchd48@gmail.com>
2021-04-30 13:01:00 +0530

    v-model for Vue 2 done

---
+++ src/components/BaseInputCustomPropAndEvent.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    type="text"
+    :value="text"
+    @input="onInput"
+    placeholder="Type something..."
+  />
+</template>
+
+<script>
+export default {
+  name: "BaseInputCustomPropAndEvent",
+  model: {
+    prop: "text",
+    event: "update",
+  },
+  props: {
+    text: {
+      type: String,
+      value: "",
+    },
+  },
+  methods: {
+    onInput(event) {
+      this.$emit("update", event.target.value);
+    },
+  },
+};
+</script>
